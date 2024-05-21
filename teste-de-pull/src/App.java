6401f9760b15b875259b77f255989ac563a7d56c
joao-santos17 <joaosantos.07417@gmail.com>
2024-05-21 19:28:07 -0300

    Commit de novo teste

--- teste-de-pull/src/App.java
+++ teste-de-pull/src/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.println("Teste de Push");
+        System.out.println("Novo teste de Push");
     }
 }
